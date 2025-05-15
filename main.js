b1ddd2da381851af8bdc2dc9a0605bcca9b827e4
Blandus1 <blandine0045@gmail.com>
2025-05-15 03:05:31 -0700

    second kata

--- main.js
+++ main.js
@@ -8,3 +8,19 @@ function evenOrOdd(number) {
 //example
 let number = 7;
 console.log(evenOrOdd(number));
+
+//second kata
+function distinct(a) {
+     let b =[];
+     let c = new Set();
+     for ( const item of a){
+       if (!c.has(item)){
+         c.add(item);
+         b.push(item);
+       }
+     }
+  return b;
+}
+//example
+let a = [12,3,4,1,1,1,2,3,3]
+console.log(distinct(a))
