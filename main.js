5ca3c71585e62620819457f3aaf88a23cdd82923
Blandus1 <blandine0045@gmail.com>
2025-05-28 01:17:44 -0700

    difficult one

--- main.js
+++ main.js
@@ -47,4 +47,22 @@ function maps(x){
  
   return x.map((num) => num*2);
 }
+//KATA
+function isOpposite(s1, s2){
+  //coding here...
+  if (s1.length === 0 || s2.length === 0 || s1.length !== s2.length) {
+    return false;
+  }
+  for (let i = 0; i < s1.length; i++) {
+    if (s1[i].toLowerCase() !== s2[i].toLowerCase()) {
+      return false;
+    }
+    
+    if (s1[i] === s2[i]) {
+      return false; 
+    }
+  }
+  return true;
+
+}
 
