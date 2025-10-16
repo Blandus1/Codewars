8f9c4c7d103832bb5a387ce7c26228165d1d476a
Blandus1 <blandine0045@gmail.com>
2025-10-16 09:07:34 +0200

    rock paper scissors

--- main-1.js
+++ main-1.js
@@ -189,3 +189,13 @@ const  removeEveryOther=(arr)=>{
 
 const instance= new Ghost()
 instance.Mycolor();
+
+
+// Rock paper scissors!
+const rps = (p1, p2) => {
+  if (p1 === p2)return "Draw!"
+  if ((p1=== 'rock' && p2==='scissors') || (p1=== 'scissors' && p2==='paper') || (p1=== 'paper' && p2==='rock')){
+    return "Player 1 won!"
+  }
+  return "Player 2 won!"
+};
