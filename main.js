8caeff39dc7e1ee58f2b1bdb9b9e917c15eb50f6
Blandus1 <blandine0045@gmail.com>
2026-02-04 08:55:50 +0200

    returning a reversed made array

--- main.js
+++ main.js
@@ -441,3 +441,12 @@ const add = (num1, num2) => {
 
   return Number(result);
 };
+
+// Kata: Reverse a sequence
+const reverseSeq = n => {
+let array=[]
+for (let i=n; i >0; --i){
+array.push(i);
+}
+  return array;
+};
