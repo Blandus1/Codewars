0bc5e9f617e00eb4cce9e8c1dd7e9ff391c262ff
Blandus1 <blandine0045@gmail.com>
2026-03-02 09:07:10 +0200

    finding the third angle

--- typeScript.js
+++ typeScript.js
@@ -1,5 +1,11 @@
 //Codewars done using TypeScript
+
 //1. KATA:Basic variable assignment
 let a: string = "code";
 let b: string ="wa.rs";
 export let name: string = a + b;
+
+//2. KATA: Finding the third angle
+export const otherAngle = (a: number, b: number): number => {
+  return (180 - (a + b));
+}
